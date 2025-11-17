fd8d7bf0435ed399429e16b3bd53bf1c69d2e32f
Code-NinjaAserft <chefaserft124@gmail.com>
2025-11-17 21:31:28 +0100

    -

--- src/main.cpp
+++ src/main.cpp
@@ -496,8 +496,6 @@ Layout computeLayout(const StyleSheet::Metrics& metrics, int windowWidth, int wi
         + layout.metrics.actionButtonHeight
         + actionPadding;
     const int summaryHeight = layout.metrics.summaryHeight;
-    const int summaryHeight = layout.metrics.summaryHeight;
-    const int summaryHeight = layout.metrics.summaryHeight;
     const int columnStart = margin;
     const int cartLeft = columnStart + categoriesWidth + columnGap + productsWidth + columnGap;
     const int cartRight = cartLeft + cartWidth;
